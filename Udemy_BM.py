4155f19e6516f15b8a408b542062f562c7b7cf3b
Xiaotong Xu <102460988+ntxxt@users.noreply.github.com>
2022-09-30 22:12:10 +0200

    Boltzmann Machines intro

---
+++ Udemy_BM.py
@@ -0,0 +1,7 @@
+#Boltzmann Machines, deep belief networks, deep Boltzmann Machines
+#no directions, no output layer, 
+#whole system together, feed input data, help to adjust the weight
+#so it actually  resemble our system
+#Restricted Boltzmann Machines
+#contrastive divergence
+##adjust the energy curve by modifying the weights to facilitate a system, which resembles our input values
